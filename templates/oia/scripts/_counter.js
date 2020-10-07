2a95840bdc6cdb20872740cab748f25dc45946c9
paul-flury <paul@flury.uk.net>
2020-10-07 20:33:23 +0100

    This is reallinh bring in whole project post addition of 2020 copyright change. Last was just the .gitignore file

---
+++ templates/oia/scripts/_counter.js
@@ -0,0 +1,5 @@
+document.write('<script type="text/javascript" src="http://s20.sitemeter.com/js/counter.js?site=s20BlueMoon">');
+document.write('</script>');
+document.write('<noscript>');
+document.write('<a href="http://s20.sitemeter.com/stats.asp?site=s20BlueMoon" target="_top">');
+document.write('<img src="http://s20.sitemeter.com/meter.asp?site=s20BlueMoon" alt="Site Meter" border="0"/></a>');
